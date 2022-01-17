a2b1df700c55e76a29a5588f1aea33f19854c73d
Oleg <aleynikov.oleg@gmail.com>
2022-01-17 12:07:04 +0300

    Dependency inversion

---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace DependencyInversion
+{
+    #region Before
+    class Book
+    {
+        public string Text { get; set; }
+        public ConsolePrinter Printer { get; set; }
+
+        public void Print()
+        {
+            Printer.Print(Text);
+        }
+    }
+
+    class ConsolePrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+    #endregion
+
+    #region After
+    interface IPrinter
+    {
+        void Print(string text);
+    }
+
+    class BookDI
+    {
+        public string Text { get; set; }
+        public IPrinter Printer { get; set; }
+
+        public BookDI(IPrinter printer)
+        {
+            this.Printer = printer;
+        }
+
+        public void Print()
+        {
+            Printer.Print(Text);
+        }
+    }
+
+    class ConsolePrinterDI : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine("Печать на консоли");
+        }
+    }
+
+    class HtmlPrinter : IPrinter
+    {
+        public void Print(string text)
+        {
+            Console.WriteLine("Печать в html");
+        }
+    }
+    #endregion
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookDI book = new BookDI(new ConsolePrinterDI());
+            book.Print();
+            book.Printer = new HtmlPrinter();
+            book.Print();
+        }
+    }
+}
